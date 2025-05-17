89ce8088c67b1035e95cb3c81f3feb71c8e0a729
Shreya990-svg <vegetam712@gmail.com>
2025-05-18 01:01:02 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -43,7 +43,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`,data)
+      .put(`https://mern-stack-project-bnty.onrender.com/books/${id}`,data)
       .then(()=>{
         setLoading(false);
         navigate('/');
