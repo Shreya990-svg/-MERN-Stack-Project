21a7de45ec196e0658906dd6dcb5b00016c0e7c8
Shreya990-svg <vegetam712@gmail.com>
2026-01-05 14:18:17 +0530

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -14,7 +14,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5000/books/${id}`)
+      .get(`https://mern-stack-project-bnty.onrender.com/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
